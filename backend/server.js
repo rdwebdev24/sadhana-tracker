64de9f9e3f0402968319d9bd23045469eafd5299
rdwebdev24 <rohitdhakad101@gmail.com>
2026-07-17 19:22:07 +0530

    frontend to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const path = require("path");
-require("dotenv").config({ path: path.join(__dirname, ".env") });
 const colors = require("colors");
 const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
 const connectDB = require("./config/db.js");
+require("dotenv").config({ path: path.join(__dirname, ".env") });
 
 const sadhanaRoutes = require("./routes/sadhanaRoutes");
 const reflectionRoutes = require("./routes/reflectionRoutes");
@@ -14,21 +14,21 @@ const dns = require('node:dns/promises');
 dns.setServers(["1.1.1.1"]);
 
 
-
 connectDB();
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "frontend")));
 
-
+console.log(express.static(path.join(__dirname, "frontend")),"aaa")
 
 app.use("/api/sadhana", sadhanaRoutes);
 app.use("/api/reflections", reflectionRoutes);
 
 
 app.get("/", (req, res) => {
-  res.send({ status: 200, msg: "success" });
+   res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
 app.listen(PORT, () => {
